fae03f79d5b135d6fb9768a648d92b5432948588
Iwona Sasak <sasak.iwona.88@gmail.com>
2018-02-17 13:23:23 +0100

    jdbc issue during start - fixed

--- springBootLearning/src/main/java/pl/iwiwiwi88/springBootLearning/SpringBootLearningApplication.java
+++ springBootLearning/src/main/java/pl/iwiwiwi88/springBootLearning/SpringBootLearningApplication.java
@@ -1,9 +1,15 @@
 package pl.iwiwiwi88.springBootLearning;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class SpringBootLearningApplication {
 
 	public static void main(String[] args) {
